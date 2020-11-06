aab732aae4755306c37ebe2956f8af317bcb7c90
pwwang <1188067+pwwang@users.noreply.github.com>
2020-11-05 17:50:51 -0700

    Update __init__.py

--- plkit/__init__.py
+++ plkit/__init__.py
@@ -7,7 +7,7 @@ from .optuna import Optuna, OptunaSuggest
 from .runner import Runner, LocalRunner, SGERunner
 from .utils import logger
 
-__version__ = "0.0.9"
+__version__ = "0.0.10"
 
 def run(config: Dict[str, Any],
         data_class: Type[DataModule],
